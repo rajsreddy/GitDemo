5d0c8c56ea1026523acf6b68f2aa06d843ff094f
rajsreddy <rsreddy1.b@gmail.com>
2021-04-15 23:27:17 +0530

    Added few more test cases

---
+++ src/test/java/qaclickacademy/SampleJavaProg.java
@@ -0,0 +1,9 @@
+package qaclickacademy;
+
+public class SampleJavaProg {
+	
+	public static void main(String[] args) {
+		System.out.println("This is sample java Program");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/TestJavaProg.java
@@ -0,0 +1,8 @@
+package qaclickacademy;
+
+public class TestJavaProg {
+	public static void main(String[] args) {
+		System.out.println("Test java Api Program");
+	}
+
+}
