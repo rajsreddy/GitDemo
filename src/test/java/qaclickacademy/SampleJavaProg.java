01e11f90bf5fe9a92f834adef9b4cecbe8402776
rajsreddy <rsreddy1.b@gmail.com>
2021-04-15 23:51:56 +0530

    few changes have done

--- src/test/java/qaclickacademy/SampleJavaProg.java
+++ src/test/java/qaclickacademy/SampleJavaProg.java
@@ -2,8 +2,21 @@ package qaclickacademy;
 
 public class SampleJavaProg {
 	
+	public void test() {
+		System.out.println("This is a beautiful test");
+	}
+	
+	public void name() {
+		System.out.println("My name is java");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is sample java Program");
+		///
+		///
+		////
+		System.out.println("This is added stuff to java Program");
+		System.out.println("this is goddamm project");
+		
 	}
-
+	
 }
