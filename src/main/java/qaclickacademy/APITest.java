f7c32390dc6baf285f7938044dd9f9bd3e9609b0
rajsreddy <rsreddy1.b@gmail.com>
2021-04-15 23:14:57 +0530

    This is commited

---
+++ src/main/java/qaclickacademy/APITest.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+public class APITest {
+	
+	public static void main(String[] args) {
+		System.out.println("This is my Java Code");
+		System.out.println("This is my API Code");
+		System.out.println("This is my Selenium Code");
+	}
+
+}
---
+++ src/main/java/qaclickacademy/SampleTest.java
@@ -0,0 +1,8 @@
+package qaclickacademy;
+
+public class SampleTest {
+	public static void main(String[] args) {
+		System.out.println("This is sample code to commit in git");
+	}
+
+}
