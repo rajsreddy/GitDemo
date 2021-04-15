d54d4ff56c8a6bbf1fe75ac8927c8f7451c01c43
rajsreddy <rsreddy1.b@gmail.com>
2021-04-15 23:59:06 +0530

    Some test case are modified

--- src/main/java/qaclickacademy/APITest.java
+++ src/main/java/qaclickacademy/APITest.java
@@ -1,7 +1,9 @@
 package qaclickacademy;
 
 public class APITest {
-	
+	public void apiTest() {
+		System.out.println("Api method");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is my Java Code");
 		System.out.println("This is my API Code");
--- src/test/java/qaclickacademy/TestJavaProg.java
+++ src/test/java/qaclickacademy/TestJavaProg.java
@@ -1,6 +1,10 @@
 package qaclickacademy;
 
 public class TestJavaProg {
+	
+	public void test1() {
+		System.out.println("test");
+	}
 	public static void main(String[] args) {
 		System.out.println("Test java Api Program");
 	}
